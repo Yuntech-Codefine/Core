bebf02eaf8addbe83c6ebbdf01d39adbe784eeff
luffyvika <luffyvika@luffyvika-THINK>
2015-10-26 20:37:06 +0800

    upload t.java

---
+++ t.java
@@ -0,0 +1,43 @@
+ import java.util.HashMap;
+a = " qqq "
+ Cyclomatic Complexity = 12class Foo<A,C,BB> {public void example ( String a[] , int b)  {
+
+	    HashMap<String, Integer>
+2       if (a == b)  {
+3           if (a1 == b1) {
+                fiddle();
+4           } else if (a2 == b2) {
+                fiddle();
+            }  else {
+                fiddle();
+            }
+5       } else if (c == d) {
+6           while (c == d) {
+                fiddle();
+            }
+7        } else if (e == f) {
+8           for (int n = 0; n < h; n++) {
+                fiddle();
+            }
+        } else{
+            switch (z) {
+9               case 1:
+                    fiddle();
+          	          break;
+10              case 2:
+                    fiddle();
+                    break;
+11              case 3:
+                    fiddle();
+                    break;
+12              default:
+	               
+                    fiddle("himom"); fiddle("fda");fiddle("fda");
+                   /*fiddle("hi"); fiddle("vika");fiddle("luffy");
+                    dff*/
+                   
+                    break;
+            }
+        }
+    }
+}public    class                                    tedst     <   tttt,    ff ,df    ,fff >   {}
