642da459050c8fe5a4117962b767bffded4310d9
Administrator <Administrator@tzang>
2015-11-27 22:53:54 +0800

    pre

--- src/codefine/metric/Halstead.java
+++ src/codefine/metric/Halstead.java
@@ -46,23 +46,23 @@ public class Halstead extends Algorithm {
 	String nameclass;
 	String escapedchar[] = {"\\","\'","\"","\b","\f","\n","\r","\t"};  // 跳脫字元
 	String keys[] = {"%b","%h","%s","%c","%d","%o","x","e","f","g","a","t"};
-    String keyschar[] = {"return","case","int", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "boolean",
-        	"double", "if",  "this", "break", "implements","throw", "byte", "else", "try", "char",
+    String keyschar[] = {"return","case", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "boolean",
+        	"do", "if",  "this", "break","double", "implements","throw", "byte", "else", "try", "char",
         	"final ","finally", "long", "const", "float",  "super", "while", "String", "interface ", "throws ", "public ", "protected ", "abstract ", "import ",
     	"static ", "void ", "class ", "strictfp ", "volatile ", "synchronized ", "private ", "native ",
     	"++","*", "+", "-", "<<", "<", "==", "&", "^", "|", "&&","||", "?:", ">=", "+=", "-=", "*=", "/=", ":",
     	"%=", "&=","^=", "|=", "<<=", ">>=", ">>>=", "--", "!=","=", "*", "/", "%", "!", "==",
-		"!=", ">", ">=", "<", "<=", "=,", "~", ">>", ">>>",";" ,"(",")", "[",  "]", "{","}","do"}; // 保留字、運算子
+		"!=", ">", ">=", "<", "<=", "=,", "~", ">>", ">>>",";" ,"(",")", "[",  "]", "{","}"}; // 保留字、運算子
     String needadd[] = {"return","case","int", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "boolean",
         	"double", "if",  "this", "break","do", "implements","throw", "byte", "else", "try", "char",
-        	"final ","finally", "long", "const", "float",  "super", "while", "String","int"};
+        	"final ","finally", "long", "const", "float",  "super", "while", "String"};
     String[] SP = {",", ".", ";", "@","\\", "/*", "*/", "\""};  // 特殊符號
     String[] Num = {"0","1","2","3","4","5","6","7","8","9"};
     String[] ann = {"@deprecated","@Override","@Deprecated","@SuppressWarnings","@suppresswarnings","@SafeVarargs","@safevarargs","@FunctionalInterface",
     		"@functionalinterface","@retentizzon","@Retention","@Docmented","@documented","@Target","@target","@Inherited","@inherited","@Repeatable","@repeatable","@Entity","@entity",
     		"@author","@Author","@Ebook","@ebook","@redonly","@Redonly","@Critical","@critical","@Schedule","@schedule","@Alert","@alert","@repeatable","@Repeatable","@interface",
     		"@NonNull","@ReadOnly","@Regex","@Tainted","@Untainted","@m"};
-    boolean decide = false;
+    		
     int countclass = 0;
     int countbig = 0, bigindex = 0;
     int countsmall = 0, smallindex = 0;
@@ -93,6 +93,7 @@ public class Halstead extends Algorithm {
 				gettest3 = fline.substring(fline.indexOf("class") + 6, fline.indexOf("<"));
 				gettest3 = gettest3.replace(" ", "");
 				nameclass = gettest3 + gettest1;
+				//System.out.println("1. " + nameclass);
 			} else {
 				String[] tokens = gettest3.split("\\s+"); // 許多空白分割成一個~
 				for(String token : tokens)
@@ -103,6 +104,7 @@ public class Halstead extends Algorithm {
 				int a2 = gettest3.indexOf(" ");
 				gettest3 = gettest3.substring(0, a2);
 				nameclass = gettest3;
+				//System.out.println("2. " + nameclass);
 			}
 		}
 	}
@@ -111,16 +113,6 @@ public class Halstead extends Algorithm {
 		int keyIndex = 0;			
 		int leftBound = -1;
 		int size = 0; // size of put
-		if (line.contains("/*")){
-			decide = true;
-		}
-		if (line.contains("*/")){
-			decide = false;
-		}
-		if ((decide == true)&&(!line.contains("*/")) ){
-			return;
-		}else{
-			
 		
 		line = line.replaceAll("\t", ""); // 將定位點取代掉
 		while((keyIndex = line.substring(leftBound + 1).indexOf("\"")) >= 0) { // 避免跳脫符號的"
@@ -149,6 +141,7 @@ public class Halstead extends Algorithm {
 						//↑好像不用這行(?)
 					}
 				} else { // ??不知道啥時會進來 
+					//System.out.println("注意注意!!!"+keyIndex);
 					put.add(keyIndex + put.get(put.size() - 1));
 					size++;
 				}
@@ -194,8 +187,9 @@ public class Halstead extends Algorithm {
 				} else {
 					operands.put(key, 1);
 				}
-			
+			//	System.out.println("\n原始雙引號：" + line);
 				line = line.substring(0, put.get(t - 1) + size - 1) + line.substring(put.get(t) + size - 1);
+			//	System.out.println("幹掉之後的：" + line);
 				size -= 2;
 			}
 		} else {
@@ -245,42 +239,39 @@ public class Halstead extends Algorithm {
 		for(int b = 0; b < ann.length; b++) { 
 			  if (line.contains(ann[b])){
 				  line=line.replace(ann[b] ," ");
+				  System.out.println("幹你老師");
 			  }
 			}
 			
 		
-	
+		String line20 = line;
 		String line21 = line;
 	
 		if(countbig != 0) {                    //假設我們要找string123
 			for (int h = 0; h < needadd.length; h++){   
 				while(line21.contains(needadd[h])){    //string123
 					
-					int aaa,left,bbb,count=0,dc=0; ;char b;
+					int aaa,left,bbb,count=0 ;char b;
 					String temp;
 					String needaddp;
 					temp = line.replaceAll(" ", "");
-					aaa = needadd[h].length();  //算出string的長度(算出來是6)		
+					aaa = needadd[h].length();  //算出string的長度(算出來是6)
+					
+					System.out.println(aaa+needadd[h]);
+					
 					left = line21.indexOf(needadd[h]);   //獲得string第一個字string的位置
-					
-					
 					int first = left + aaa;  //第一個應該要判斷的
 					
 					if (aaa == temp.length()){
 						break;
 					}
-					/*if (dc > 0){
-					dc= left -1;
-					if (line.charAt(dc) != ' '){
-						break;
 					
-					}
-					}*/
+					System.out.println(aaa+needadd[h]);
+					System.out.println(line.length());
 					b = line21.charAt(first); 
 					int ccc = first;
 					int ccc1 = 0;
 					while(ccc1 != ccc){  //如果算出來的b值不是空白的話
-						
 						ccc1 = ccc;      //轉char成String
 						bbb = Integer.valueOf(b);  //String 轉成int
 						for (int aa=48; aa<58; aa++){ //0~9的阿斯ㄎㄧ碼
@@ -290,6 +281,8 @@ public class Halstead extends Algorithm {
 									    ccc = ccc + 1 ;
 										b=line21.charAt(ccc);
 										count= count +1;
+										
+										//break;
 									}
 							}
 										for(int at=97; at<123; at++){
@@ -297,7 +290,7 @@ public class Halstead extends Algorithm {
 											if (bbb == at ){        //是否等於a-z
 											
 												 ccc = ccc+1 ;
-												
+												 System.out.println(line+" "+ at);
 												b=line21.charAt(ccc);
 												
 												count= count +1;
@@ -318,68 +311,43 @@ public class Halstead extends Algorithm {
 									}//A~Z的最後
 										
 								}
-					
 						int total =0;
 						total = count + left+aaa; 
+						
+						System.out.println(line);
 						needaddp = line21.substring(left,total);
+						System.out.println(needaddp);
 						if(operands.containsKey(needaddp)) {
 							operands.put(needaddp, operands.get(needaddp) + 1);
-							line21 = line21.replace(needaddp, " ");
 						} else {
 							operands.put(needaddp, 1);
-							line21 = line21.replace(needaddp, " ");
 						}
-						
-						
+						line21 = line21.substring(0,(left))+ line21.substring(total);
+						System.out.println(line21);
 						}
 						
 					}
-			String line20 = line21;
-		
+			
 			for(int i = 0; i < keyschar.length; i++) { //從第一個保留字開始
-				line21 = line20;
-				System.out.println(i + "  " +line21);
 				int here = 0;
 				 //String t1 = keyschar[i].substring(keyschar[i].length()-1); //抓字串最後一字 , 好比class ,最後一個是空白
-			/*if (keyschar[i].length() >= line21.length()){
+			/*if (keyschar[i].length() >= line.length()){
 					break;
 				}*/
-			int key,bc;			
-			key = line21.indexOf(keyschar[i]);
-			bc = key -1;
-			int cd = key +1;
-			/*if (bc > 0){
-				 if (line21.charAt(bc) != ' '){
-						break;
-				 }
-			}*/
-			
-			
-					 while(line21.contains(keyschar[i])) {
-						 System.out.println(line21);
+					 while(line20.contains(keyschar[i])) {
 						    operators.put(keyschar[i], operators.get(keyschar[i]) + 1);
-							here = line21.indexOf(keyschar[i]);
+							here = line20.indexOf(keyschar[i]);
 							here = here + 1;
-							line21 = line21.replace(keyschar[i],"");
-							System.out.println("After"+line21);
+							line20 = line20.substring(here);
 						 }
+				line20 = line;
 				
-				line21 = line;
 			}
 			
-		
-			for(int replacekey = 0 ; replacekey < keyschar.length; replacekey++){
-				/*int key,bc;			
-				key = line21.indexOf(keyschar[replacekey]);
-				bc = key -1;
-				if (bc > 0){
-					if (line21.charAt(bc) != ' '){
-						break;
-					}
-				}*/
-				
+			
+			for(int replacekey = 0 ; replacekey < keyschar.length; replacekey++)
 	            line = line.replace(keyschar[replacekey], " ");
-		}
+			
 			for(int b = 0; b < SP.length; b++)
 				line = line.replace(SP[b], " ");
 			
@@ -414,6 +382,8 @@ public class Halstead extends Algorithm {
 		}
 		
 		if((countbig == 0) && (operands.size() != 0)) {
+			 
+				
 			int n1 = 0, N1 = 0;
 			int n2 = 0, N2 = 0;
 			System.out.println("\nclass name: "+ nameclass);
@@ -442,7 +412,7 @@ public class Halstead extends Algorithm {
 	        operands.clear();
 		}
 	}
-	}
+	
 	public String getValue() {
 		JSONArray jsonArr = new JSONArray();
 		
